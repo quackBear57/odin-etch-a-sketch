abbc63d2288f1cfb15501fe5696773db2274b9db
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-02-06 10:13:51 -0600

    add button, edits to sizing and alignment

--- code.js
+++ code.js
@@ -1,4 +1,14 @@
-const gameContainer = document.querySelector("#game-container");
+const button = document.querySelector("#btnSize");
+const container = document.querySelector(".content");
+
+container.style.display = "flex";
+container.style.flexDirection = "column";
+container.style.gap = "10px";
+container.style.alignItems = "center";
+// button.style.flex = 0;
+button.style.width = "100px";
+
+const gameContainer = document.querySelector("#gameContainer");
 
 gameContainer.style.display = "flex";
 gameContainer.style.flexDirection = "column";
@@ -6,6 +16,8 @@ gameContainer.style.alignItems = "center";
 
 let numberRows = 4;
 let numberColumns = numberRows;
+let maxWidth = 600; //pixels
+let maxHeight = 600; //pixels
 
 for (let i = 0; i < numberRows; i++) {
     let row = document.createElement("div");
@@ -15,8 +27,8 @@ for (let i = 0; i < numberRows; i++) {
     for (let j = 0; j < numberColumns; j++) {
         let square = document.createElement("div");
         square.classList.add("square");
-        square.style.minHeight = "100px";
-        square.style.minWidth = "100px";
+        square.style.minHeight = maxHeight/numberColumns + "px";
+        square.style.minWidth = maxWidth/numberColumns + "px";
         square.style.border = "solid black 2px"
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = "pink";
